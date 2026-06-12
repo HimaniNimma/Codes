689e9a1222947b17a40574eccb1739046a3262e3
Himani Nimma <it1298himani@gmail.com>
2026-06-12 12:14:43 +0530

    Refactor vowel counting logic in string

--- 10.-Vowels count in a string.py
+++ 10.-Vowels count in a string.py
@@ -2,6 +2,6 @@
 h=input("enter string")
 vowels=0
 for i in h:
-    if(i=='a'or i=='e'or i=='i'or i=='o'or i=='u'or i=='A'or i=='E'or i=='I'or i=='O'or i=='U'):
+    if i in "aeiouAEIOU":
         vowels=+1
 print(vowels)
